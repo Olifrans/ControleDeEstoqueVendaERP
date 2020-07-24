29ab5ab7dcbafd0b51bc7cdcaa6c3f9aed914b91
FRANCISCO OLIVEIRA <fransoliveira@hotmail.com>
2020-07-24 16:12:28 -0300

    Novos dados

--- DAL/DadosDaConexao.cs
+++ DAL/DadosDaConexao.cs
@@ -11,7 +11,7 @@ namespace DAL
     {
         public static String StringDeConexao
         {
-            get //Devolve os dados de conexao que utilizaremos
+            get //Devolve os dados de conexao que utilizaremos no projeto
             {
                 return "Data Source=DESKTOP-SBPGI7N;Integrated Security=SQLOLEDB.1; Integrated Security = SSPI; Initial Catalog = ControleDeEstoque";
             }
